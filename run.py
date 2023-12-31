968177e89d6d8eb92d87ac3a5e6d0a473b01b3f3
Joey Gallegos <joey@joeygallegos.com>
2023-12-30 23:27:16 -0600

    Working code

---
+++ run.py
@@ -0,0 +1,97 @@
+import schedule
+import time
+import os
+import configparser
+import mysql.connector
+import subprocess
+
+
+def setup_db():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(script_dir, "config.ini")
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    mysql_config = {
+        "host": config.get("MYSQL", "HOST"),
+        "port": config.getint("MYSQL", "PORT"),
+        "user": config.get("MYSQL", "USER"),
+        "password": config.get("MYSQL", "PASSWORD"),
+        "database": config.get("MYSQL", "DATABASE_NAME"),
+    }
+    conn = mysql.connector.connect(**mysql_config)
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+CREATE TABLE IF NOT EXISTS orders
+(
+    id BIGINT(64) AUTO_INCREMENT PRIMARY KEY,
+    source TEXT,
+    command_uuid VARCHAR(36),
+    used TINYINT(1) DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    used_at TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+                   """
+    )
+    conn.commit()
+    return conn, cursor
+
+
+# Local database of simple command names and their corresponding Windows CLI commands
+COMMANDS_DICT = {
+    "25c173df-952f-40e7-94a6-7f6ba77ea01e": 'shutdown /s /t 60 /c "PythonRemoteCommandExecutor: shutdown" /d U:5:19',
+    "6e495198-17f1-455c-b44e-eb620c19fab1": 'shutdown /r /t 60 /c "PythonRemoteCommandExecutor: reboot" /d U:5:19',
+}
+
+
+# Define a function to check the MySQL server for commands and execute them
+def check_and_execute_command():
+    command_to_run = None
+    print("Checking for commands")
+    try:
+        conn, cursor = setup_db()
+
+        # Query for commands where "used" = 0
+        cursor.execute("SELECT command_uuid FROM orders WHERE used = 0 LIMIT 1;")
+        commands = cursor.fetchall()
+
+        for command in commands:
+            # Execute the command (you can modify this part)
+            # For example, print the command for demonstration purposes
+            print("Executing command:", command[0])
+
+            # Update the "used" flag to 1
+            cursor.execute(
+                "UPDATE orders SET used = 1, used_at = CURRENT_TIMESTAMP WHERE command_uuid = %s;",
+                (command[0],),
+            )
+            conn.commit()
+
+            # Look up the simple command in the local database
+            command_to_run = COMMANDS_DICT.get(command[0])
+
+            if command_to_run:
+                print(f"Executing command: {command_to_run}")
+            else:
+                print(f"Command {command_to_run} not found in the local database.")
+
+        cursor.close()
+        conn.close()
+
+        # If command to run present
+        if command_to_run is not None:
+            # Execute the command
+            subprocess.run(command_to_run, shell=True)
+
+    except Exception as e:
+        print("Error:", str(e))
+
+
+# Schedule the check_and_execute_commands function to run every minute
+schedule.every(1).minutes.do(check_and_execute_command)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
